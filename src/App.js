48eada00430aaf46879b4a5ab4689b43ab8383bb
jorgelucasalima <jorgelucasalima@gmail.com>
2021-07-12 23:32:39 -0300

    Criação da routes.js e styles.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+//API DO PROJETO 
+// https://sujeitoprogramador.com/r-api/?api=filmes/
+
+import Routes from "./routes"
+import './styles.css'
+
 
 export default function App() {
     return (
       <div>
-        <h1>Filmaria</h1>
+        <Routes/>
       </div>
     )
 }
+

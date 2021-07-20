797f33f79552485c8bba6a5621ed61acc190f05a
jorgelucasalima <jorgelucasalima@gmail.com>
2021-07-19 23:01:09 -0300

    criado pagina de erro 404 e projeto finalizado

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 import Favoritos from './pages/Favoritos'
+import Erro from './pages/Erro'
 
 
 const Routes = () => {
@@ -17,6 +18,7 @@ const Routes = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/filme/:id" component={Filme} />
                 <Route exact path="/favoritos" component={Favoritos} />
+                <Route exact path="*" component={Erro} />
             </Switch>
         </BrowserRouter>
     )
